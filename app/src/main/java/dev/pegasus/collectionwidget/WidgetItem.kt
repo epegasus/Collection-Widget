437560b2527de9961373eb6516032f5bcd249ccf
Sohaib Ahmed <sohaibmughal93@gmail.com>
2024-09-18 23:26:39 +0500

    Collection Widget

---
+++ app/src/main/java/dev/pegasus/collectionwidget/WidgetItem.kt
@@ -0,0 +1,13 @@
+package dev.pegasus.collectionwidget
+
+/**
+ * @Author: SOHAIB AHMED
+ * @Date: 17/09/2024.
+ * @Accounts
+ *      -> https://github.com/epegasus
+ *      -> https://linkedin.com/in/epegasus
+ */
+
+data class WidgetItem(
+    val text: String
+)
